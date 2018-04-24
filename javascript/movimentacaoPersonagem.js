649ab523d03be6edb362276857a7d9ee22b7d30a
MarcoSants <marcochel@gmail.com>
2018-04-23 21:15:44 -0300

    Personagem mov com animacoes gif e limites de mov ajustados

--- javascript/movimentacaoPersonagem.js
+++ javascript/movimentacaoPersonagem.js
@@ -1,21 +1,33 @@
 var madoka = document.getElementById('madoka');
-var maxX = screen.width;
-//Tem que fazer o controle pra ela nao conseguir deslocar para fora da tela
-
 var posicaoX = 0;
+var imagemEscolhida = 0;
 
-//document.onkeydown = movimentaX;
-document.onkeypress = movimentaX;
+document.onkeydown = movimentandoX;
+document.onkeyup = paradaX;
 
-function movimentaX(e) {
+function movimentandoX(e) {
     if(e.keyCode==39){
-        madoka.src="img/madokaDireita.gif";
-        posicaoX += 5
+        if (imagemEscolhida !== 1) {
+            madoka.src = "img/madokaDireita.gif";
+            imagemEscolhida = 1;
+        }
+        let tamanhoTela = window.innerWidth;
+        if (!(posicaoX + 5 > tamanhoTela))
+            posicaoX += 5;
         madoka.style.left = posicaoX + 'px';
     }
     if(e.keyCode==37){
-        madoka.src="img/madokaEsquerda.gif";
-        posicaoX -= 5
+        if (imagemEscolhida !== 2) {
+            madoka.src = "img/madokaEsquerda.gif";
+            imagemEscolhida = 2;
+        }
+        if (!(posicaoX - 5 < 0))
+            posicaoX -= 5;
         madoka.style.left = posicaoX + 'px';
     }
+}
+
+function paradaX() {
+    madoka.src = "img/madokaParada.gif";
+    imagemEscolhida = 0;
 }
