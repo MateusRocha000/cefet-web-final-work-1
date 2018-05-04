c441f01730c18247e04dc9e846ef7a9f0be1895a
MateusRocha000 <mateusrochagoncalves@gmail.com>
2018-05-03 22:28:13 -0300

    Sem placa/Botão em pt-br

--- javascript/code.js
+++ javascript/code.js
@@ -1,13 +1,33 @@
 var madoka = document.getElementById('madoka');
+var wall1 = document.getElementById('wall1');
+
 var posicaoX = 0;
 var imagemEscolhida = 0;
 
 document.onkeydown = movimentandoX;
 document.onkeyup = paradaX;
 
+//localStorage.posicaoX = 0;
+
+if(localStorage.posicaoX !== "" && localStorage.posicaoX !== undefined  && !isNaN(localStorage.posicaoX))
+{
+    posicaoX = parseInt(localStorage.posicaoX);
+    madoka.style.left = posicaoX + 'px';
+}
+
+if(localStorage.imagemEscolhida !== ""  && localStorage.imagemEscolhida !== undefined  && !isNaN(localStorage.imagemEscolhida))
+{
+    imagemEscolhida = parseInt(localStorage.imagemEscolhida);
+    if (imagemEscolhida === 2)
+        madoka.src = "img/madokaParadaEsquerda.gif";
+    else if (imagemEscolhida === 1)
+        madoka.src = "img/madokaParadaDireita.gif";
+}
+
+
 function movimentandoX(e) {
     if(e.keyCode==39){
-        if (imagemEscolhida !== 1) {
+        if (imagemEscolhida !== 1 || madoka.src.includes("img/madokaParadaDireita.gif")) {
             madoka.src = "img/madokaDireita.gif";
             imagemEscolhida = 1;
         }
@@ -15,9 +35,10 @@ function movimentandoX(e) {
         if (!(posicaoX + 5 > tamanhoTela))
             posicaoX += 5;
         madoka.style.left = posicaoX + 'px';
+        
     }
     if(e.keyCode==37){
-        if (imagemEscolhida !== 2) {
+        if (imagemEscolhida !== 2 || madoka.src.includes("img/madokaParadaEsquerda.gif")) {
             madoka.src = "img/madokaEsquerda.gif";
             imagemEscolhida = 2;
         }
@@ -25,9 +46,13 @@ function movimentandoX(e) {
             posicaoX -= 5;
         madoka.style.left = posicaoX + 'px';
     }
+    localStorage.imagemEscolhida = imagemEscolhida;
+    localStorage.posicaoX = posicaoX;
 }
 
 function paradaX() {
-    madoka.src = "img/madokaParada.gif";
-    imagemEscolhida = 0;
+    if (imagemEscolhida === 2)
+        madoka.src = "img/madokaParadaEsquerda.gif";
+    else if (imagemEscolhida === 1)
+        madoka.src = "img/madokaParadaDireita.gif";
 }
